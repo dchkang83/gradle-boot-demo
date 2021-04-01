1485976b3ee33b103d3f46602e0af29a4d15d320
강덕준 <dchkang83@naver.com>
2021-04-01 18:21:36 +0900

    test commit

---
+++ src/main/java/com/example/gradlebootdemo/GradleBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlebootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradlebootdemo/GradleBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlebootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
